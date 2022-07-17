cfc4426d6cc010c06ebb1f4003f9799acf2a0c06
Angel Acosta <angel.s.acosta1@gmail.com>
2022-07-17 15:11:52 -0400

    added material UI; began menu

--- src/components/nav-bar/Nav.js
+++ src/components/nav-bar/Nav.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './nav.scss';
+import {Menu} from "@mui/icons-material"
 
 export default function Nav() {
   return (
     <div className="nav">
       <div className="wrapper">
         <div className="left">
-          <a href="#home">Home</a>
+          <a href="#home" className="logo">
+            {`< A A / >`}
+            {/* <img src= "public/portfolio_logos/angel-logo-2.png" alt="logo"></img> */}
+          </a>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="menu">
+            <Menu/>
+            {/* <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span> */}
+          </div>
+        </div>
       </div>
     </div>
   );
