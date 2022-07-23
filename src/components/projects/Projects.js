830def27975a2618c4171c5021876f9c36f07314
Angel Acosta <angel.s.acosta1@gmail.com>
2022-07-22 23:12:37 -0400

    added project photoes & project page

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,39 @@
-import React from "react";
-import './projects.scss'
+import React from 'react';
+import './projects.scss';
+import { projects } from '../../projectData';
 
 export default function Projects() {
   return (
-    <div className = "projects" id = "projects">
+    <div className="projects" id="projects">
       <h1>Projects</h1>
-      
+      <div className="container">
+        {projects.map((project) => (
+          <div className="card">
+            <div className="top">
+              <h2>{project.name}</h2>
+            </div>
+            <div className="center">
+              <img alt="" src={project.img}></img>
+            </div>
+            <div className="bottom">
+              <div className="left">
+                <button>
+                  <a target="_blank" rel="noreferrer" href={project.github}>
+                    GitHub
+                  </a>
+                </button>
+              </div>
+              <div className="right">
+                <button>
+                  <a target="_blank" rel="noreferrer" href={project.live}>
+                    Live
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/projectData.js
@@ -0,0 +1,15 @@
+export const projects = [
+  {
+    name: 'Daja View',
+    github: 'https://github.com/sealotter/capstone-project',
+    live: 'https://capstone-movie-social-app.herokuapp.com/login',
+    img: 'portfolio_logos/DajaView.png'
+  }, 
+  {
+    name: 'Music-R-Us',
+    github: 'https://github.com/Jzhang1225/Music-R-Us',
+    live: 'http://music-r-us-final.herokuapp.com/',
+    img: 'portfolio_logos/Music-R-Us.png'
+  }, 
+  
+]
